ae4ac6849b30f08e398a8f31fc1b9fd507c57a29
Aleksei Waddington <amw694@nyu.edu>
2020-08-30 19:03:31 -0400

    Blocked out main menu and added a scrolling credits section

---
+++ Trip/Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject scrollRectObject;
+    ScrollRect scrollRect;
+
+    public float distance = 500f;
+    public float currentY = -552f;
+    public float speedIncrease = 0.001f;
+
+    private bool _moveUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect = scrollRectObject.GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_moveUp)
+        {
+            if (currentY <= distance)
+            {
+                currentY += speedIncrease;
+            }
+            else
+            {
+                _moveUp = false;
+            }
+
+            var pos = new Vector2(0f, currentY * 100f);
+            scrollRect.content.localPosition = pos;
+        }
+    }
+
+    public void setMoveBool(bool activate)
+    {
+        _moveUp = activate;
+    }
+}
---
+++ Trip/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+
+    private Credits _creditsScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _creditsScript = CreditsPanel.GetComponent<Credits>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateCredits()
+    {
+        CreditsPanel.SetActive(true);
+        enableMovingCredits();
+    }
+
+    void enableMovingCredits()
+    {
+        _creditsScript.setMoveBool(true);
+    }
+}
