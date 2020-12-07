22115814dadc5f380a9e49d990c933103ce8e266
Aleksei Waddington <amw694@nyu.edu>
2020-12-06 23:24:46 -0500

    more work on the text transition screen

---
+++ Trip/Assets/Scripts/TextScene/Blinker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blinker : MonoBehaviour
+{
+
+    float currentTime = 0.0f;
+    float speed = 1f;
+    Image blinkerImage;
+    int index = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blinkerImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime * speed;
+        if (currentTime > 0.5)
+        {
+            blinkerImage.enabled = (index % 2 == 0);
+            currentTime = 0;
+            index += 1;
+        }
+    }
+}
--- Trip/Assets/Scripts/TextScene/TransitionScreenText.cs
+++ Trip/Assets/Scripts/TextScene/TransitionScreenText.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class TransitionScreenText : MonoBehaviour
 {
-    public enum TextState { Wait, Type, ResetTimer, Complete, Reset };
+    public enum TextState { Wait, Type, ResetTimer, Complete, WaitForCompletedAction, Reset, FadeIn, FadeOut, };
     public TextState currentTextState;
 
     float lastStateChange = 0.0f; //tracks time that has passed since the last state change
@@ -14,13 +14,28 @@ public class TransitionScreenText : MonoBehaviour
     public string testString = "this is a test string";
     string currentTextString = "";
     int index = 0;
+    string colorRichText = "";
+
+    public bool alphaDisplay = true;
 
     public Text textObject;
+    public Image backgroundImage;
+    public GameObject blinker;
+
+    int indexAlpha = 0;
+    string alphaText = "";
+
+
+    float alphaTime = 0.0f;
+    float startingAlpha = 0f;
+    float endingAlpha = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        colorRichText = getTextColor();
+        alphaText = colorRichText + testString + "</color>";
+        textObject.text = alphaText;
     }
 
     // Update is called once per frame
@@ -38,7 +53,17 @@ public class TransitionScreenText : MonoBehaviour
                 break;
 
             case TextState.Type:
-                displayText();
+                if(alphaDisplay)
+                {
+                    
+                    Debug.Log(colorRichText);
+                    displayTextAlpha();
+                }
+                else
+                {
+                    displayText();
+                }
+                
                 break;
 
             case TextState.ResetTimer:
@@ -46,11 +71,24 @@ public class TransitionScreenText : MonoBehaviour
                 break;
 
             case TextState.Complete:
+                activateBlinker();
+                setCurrentState(TextState.WaitForCompletedAction);
+                break;
 
+            case TextState.WaitForCompletedAction:
+                waitForClick();
                 break;
 
             case TextState.Reset:
                 resetString();
+                break;
+
+            case TextState.FadeIn:
+                _increaseImageAlpha();
+                break;
+
+            case TextState.FadeOut:
+                _decreaseImageAlpha();
                 break;
         }
 
@@ -79,6 +117,40 @@ public class TransitionScreenText : MonoBehaviour
         
     }
 
+    public void displayTextAlpha()
+    {
+        //ColorUtility.ToHtmlStringRGBA( myColor )
+       
+        if( indexAlpha >= testString.Length)
+        {
+            currentTextString = testString;
+            textObject.text = currentTextString;
+            setCurrentState(TextState.Complete);
+        }
+        else
+        {
+            if (getStateElapsed() > waitTime)
+            {
+
+                char nextChar = alphaText[colorRichText.Length + indexAlpha];
+                alphaText = alphaText.Remove(colorRichText.Length + indexAlpha, 1);
+                alphaText = alphaText.Insert(indexAlpha, nextChar.ToString());
+                textObject.text = alphaText;
+                indexAlpha += 1;
+                Debug.Log(indexAlpha);
+                setCurrentState(TextState.ResetTimer);
+            }
+               
+        }
+
+
+    }
+
+    public string getTextColor()
+    {
+        return "<color=#" + ColorUtility.ToHtmlStringRGB(textObject.color) + "00>";
+    }
+
     /// <summary>
     /// sets the current state of the game manager
     /// </summary>
@@ -105,5 +177,80 @@ public class TransitionScreenText : MonoBehaviour
         index = 0;
         textObject.text = "";
         setCurrentState(TextState.Type);
+    }
+
+    void activateTyping()
+    {
+        setCurrentState(TextState.Type);
+    }
+
+    void activateFadeIn()
+    {
+        setCurrentState(TextState.FadeIn);
+    }
+
+    void activateFadeOut()
+    {
+        setCurrentState(TextState.FadeOut);
+    }
+
+    void _increaseImageAlpha()
+    {
+        alphaTime += 0.5f * Time.deltaTime;
+        if (alphaTime < 1f)
+        {
+            float alpha = Mathf.Lerp(startingAlpha, endingAlpha, alphaTime);
+            changeImageAlpha(alpha);
+        }
+        else
+        {
+            changeImageAlpha(endingAlpha);
+            alphaTime = 0;
+            activateTyping();
+        }
+
+    }
+
+    void _decreaseImageAlpha()
+    {
+        alphaTime += 0.5f * Time.deltaTime;
+        if (alphaTime < 1f)
+        {
+            float alpha = Mathf.Lerp(endingAlpha, startingAlpha, alphaTime);
+            changeImageAlpha(alpha);
+        }
+        else
+        {
+            changeImageAlpha(startingAlpha);
+            alphaTime = 0;
+            setCurrentState(TextState.Wait);
+        }
+    }
+
+    void changeImageAlpha(float newAlpha)
+    {
+        Color tempColor = backgroundImage.color;
+        tempColor.a = newAlpha;
+        backgroundImage.color = tempColor;
+    }
+
+    void waitForClick()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            disableBlinker();
+            textObject.gameObject.SetActive(false);
+            setCurrentState(TextState.FadeOut);
+        }
+    }
+
+    void activateBlinker()
+    {
+        blinker.SetActive(true);
+    }
+
+    void disableBlinker()
+    {
+        blinker.SetActive(false);
     }
 }
